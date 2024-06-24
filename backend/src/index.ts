74d1769315a90def95f0e75c93cbd3231ad41026
See-Its-Manish <seeitsmanish@gmail.com>
2024-06-25 02:39:16 +0530

    refactor: cleanup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,14 @@
 import express from 'express';
 import { WebSocket, WebSocketServer } from 'ws';
+import { PORT } from './config/serverConfig';
+import { setUpWebSocketServer } from './webSocketServer';
 
 const app = express();
 
-const httpServer = app.listen(8080, () => {
-    console.log("Server started at 8080")
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
 });
 
-const wss = new WebSocketServer({ server: httpServer });
+setUpWebSocketServer(httpServer);
 
-wss.on('connection', function connection(webSocketInstance) {
-    webSocketInstance.on('error', err => console.error(err));
 
-    webSocketInstance.on('message', function message(data) {
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        })
-    })
-
-    webSocketInstance.send("Hello! Message from Server!!");
-})
