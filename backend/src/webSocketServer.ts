b862b66eb1f6100c7960af5694327f4365875880
See-Its-Manish <seeitsmanish@gmail.com>
2024-06-26 02:38:00 +0530

    refactor: Moved Room related functions into different file and created class RoomManager

--- backend/src/webSocketServer.ts
+++ backend/src/webSocketServer.ts
@@ -1,13 +1,8 @@
-import { WebSocket, WebSocketServer } from "ws"
+import WebSocket, { WebSocketServer } from "ws"
 import { Server } from 'http';
-import { Request } from "express";
+import { RoomManager } from "./RoomManager";
 
-export type Room = {
-    name: string;
-    clients: Set<WebSocket>;
-}
-
-const rooms: Map<string, Room> = new Map();
+const roomManagerInstance = RoomManager.getInstance();
 
 export const setUpWebSocketServer = (httpServer: Server) => {
     const wss = new WebSocketServer({ server: httpServer });
@@ -19,43 +14,24 @@ export const setUpWebSocketServer = (httpServer: Server) => {
         socketInstance.on('error', console.error);
 
         // When a client connects, get the roomName and add it to rooms
-        const roomName = extractRoomUrl(request);
-        joinRoom(socketInstance, roomName);
+        const roomName = roomManagerInstance.extractRoomUrl(request);
+        roomManagerInstance.joinRoom(socketInstance, roomName);
 
         // broadcast the message to all the members in room
         socketInstance.on('message', (data) => {
             const message = data.toString();
-            broadcastMessageInRoom(roomName, message);
+            roomManagerInstance.broadcastMessageInRoom(roomName, message);
+        })
+
+        socketInstance.on('close', () => {
+            roomManagerInstance.deleteSocketFromRoom(socketInstance, roomName);
         })
     })
 }
 
-function extractRoomUrl(request: Request) {
-    const requestArray = request.url?.split('/');
-    if (requestArray.length > 2) return null;
-    return requestArray[1];
-}
 
-function joinRoom(socketInstance: WebSocket, roomName: string | null) {
-    if (!roomName) return;
-    let room = rooms.get(roomName);
-    if (!room) {
-        room = { name: roomName, clients: new Set() };
-    }
-    room.clients.add(socketInstance);
-    rooms.set(roomName, room);
-}
 
-function broadcastMessageInRoom(roomName: string | null, message: string) {
-    if (!roomName) return;
-    const room = rooms.get(roomName);
-    room?.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        }
-    })
-}
 
 setInterval(() => {
-    console.log(rooms);
+    roomManagerInstance.logState();
 }, 5000)
