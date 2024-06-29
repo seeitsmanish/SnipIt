ab24b753fe18454dc6147fc403a42edc353a06dd
See-Its-Manish <seeitsmanish@gmail.com>
2024-06-29 18:57:47 +0530

    feat: Added gradient button

--- frontend/src/ui/Button.tsx
+++ frontend/src/ui/Button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva("rounded-md active:scale-[0.9] duration-500", {
       primary: "bg-cyan-300 text-black hover:bg-cyan-200",
       secondary: "bg-secondary text-black hover:bg-secondary-hover",
       outline:
-        "border-2 border-outline bg-outline text-black hover:bg-outline-hover",
+        "border-2 border-white bg-transparent text-white hover:bg-opacity-50",
     },
     size: {
       sm: "text-sm px-2 py-1",
@@ -33,7 +33,7 @@ const buttonVariants = cva("rounded-md active:scale-[0.9] duration-500", {
   },
 });
 
-const Button: React.FC<ButtonProps> = ({
+export const Button: React.FC<ButtonProps> = ({
   children,
   variant = "primary",
   size,
@@ -50,4 +50,59 @@ const Button: React.FC<ButtonProps> = ({
   );
 };
 
-export default Button;
+const buttonWithGradientVariants = cva(
+  "rounded-md active:scale-[0.9] duration-500 p-[2px]",
+  {
+    variants: {
+      variant: {
+        primary:
+          "bg-gradient-to-r from-cyan-300 via-blue-300 to-green-300 text-black hover:bg-cyan-200",
+        secondary: "bg-secondary text-black hover:bg-secondary-hover",
+        outline:
+          "bg-gradient-to-r from-cyan-300 via-blue-300 to-green-300 text-white",
+      },
+      size: {
+        sm: "text-sm",
+        md: "text-base",
+        lg: "text-xl",
+      },
+    },
+
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  },
+);
+
+export const ButtonWithGradient: React.FC<ButtonProps> = ({
+  children,
+  variant = "primary",
+  size,
+  className,
+  ...rest
+}) => {
+  if (variant === "outline") {
+    return (
+      <div
+        className={cn(buttonWithGradientVariants({ variant, size, className }))}
+      >
+        <button
+          {...rest}
+          className="h-full w-full rounded-md bg-black px-2 py-3"
+        >
+          {children}
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      {...rest}
+      className={cn(buttonWithGradientVariants({ variant, size, className }))}
+    >
+      {children}
+    </button>
+  );
+};
