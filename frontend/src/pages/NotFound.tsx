9815bce9d0ee4e67f71c53872243b2d691f1e192
See-Its-Manish <seeitsmanish@gmail.com>
2024-06-30 19:04:40 +0530

    feat: Not Found page for invalid routes

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, ButtonWithGradient } from "../ui/Button";
+import { Link } from "react-router-dom";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 md:mt-[30px]">
+      <h1 className="mt-[100px] bg-gradient-to-r from-blue-300 to-green-300 bg-clip-text text-[35vw] font-bold leading-[120px] text-transparent md:text-[250px] md:leading-[200px]">
+        404
+      </h1>
+      <h1 className="text-[7vw] uppercase text-cyan-100 md:text-[45px]">
+        Page Not Found
+      </h1>
+      <div className="text-center text-base font-thin text-gray-300 md:text-2xl">
+        <p>Sorry! The page you are looking for doesn't exist</p>
+        <p>May be you have entered wrong room name</p>
+      </div>
+      <div className="absolute bottom-5 flex gap-5 md:static">
+        <Link to="/">
+          <ButtonWithGradient variant="outline" className="w-[170px]">
+            Home
+          </ButtonWithGradient>
+        </Link>
+        <Button variant="primary">Create New Room</Button>
+      </div>
+    </div>
+  );
+};
+export default NotFound;
