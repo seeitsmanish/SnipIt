0a67c0083b811f66585889040627708712d48880
See-Its-Manish <seeitsmanish@gmail.com>
2024-07-01 22:45:01 +0530

    chore: Added CodeEditor in room component

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import CodeEditor from "../components/CodeEditor/CodeEditor";
 type RoomProps = {};
 
 const Room: React.FC<RoomProps> = () => {
   const { roomSlug } = useParams();
-  console.log(roomSlug);
-  return <div className="text-white">Hiiii There</div>;
+
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="flex-1">
+        <CodeEditor />
+      </div>
+    </div>
+  );
 };
 export default Room;
