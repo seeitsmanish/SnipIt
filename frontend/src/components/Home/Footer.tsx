c4ac9ead7a349529f81296ec79e40e0b747ca4ac
See-Its-Manish <seeitsmanish@gmail.com>
2024-07-01 22:43:42 +0530

    style: scale-1.2 on hover

--- frontend/src/components/Home/Footer.tsx
+++ frontend/src/components/Home/Footer.tsx
@@ -16,25 +16,28 @@ const Footer: React.FC<FooterProps> = ({}) => {
           <a href="https://www.linkedin.com/in/seeitsmanish/" target="_blank">
             <img
               src={linkedinImgSrc}
-              className="size-[30px] cursor-pointer duration-300 md:size-[40px]"
+              className="size-[30px] cursor-pointer duration-300 hover:scale-[1.2] md:size-[40px]"
             />
           </a>
           <a href="https://www.instagram.com/seeitsmanish/" target="_blank">
             <img
               src={instagramImgSrc}
-              className="size-[30px] cursor-pointer duration-300 md:size-[40px]"
+              className="size-[30px] cursor-pointer duration-300 hover:scale-[1.3] md:size-[40px]"
             />
           </a>
           <a href="https://x.com/seeitsmanish" target="_blank">
             <img
               src={twitterImgSrc}
-              className="size-[30px] cursor-pointer duration-300 md:size-[40px]"
+              className="size-[30px] cursor-pointer duration-300 hover:scale-[1.2] md:size-[40px]"
             />
           </a>
         </div>
         <div className="flex cursor-pointer items-center gap-2 text-gray-400 duration-300 hover:text-white">
           <span className="md:text-xl">Made with </span>
-          <img src={heartImgSrc} className="size-[20px] md:size-[40px]" />
+          <img
+            src={heartImgSrc}
+            className="size-[20px] duration-300 hover:scale-[1.2] md:size-[40px]"
+          />
           <span className="text-xl"> by Manish</span>
         </div>
       </div>
