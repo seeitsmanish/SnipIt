88622acf587008613471a012fd446e41097f74c4
See-Its-Manish <seeitsmanish@gmail.com>
2024-07-03 00:29:51 +0530

    impr: redirected * paths to not-found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 import Nav from "./components/Home/Nav";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Room from "./pages/Room";
 import NotFound from "./pages/NotFound";
 function App() {
@@ -15,7 +20,9 @@ function App() {
           {/* Room */}
           <Route path="/:roomSlug" element={<Room />} />
           {/* Not-Found */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/not-found" element={<NotFound />} />
+          {/* Redirect unmatched routes to not-found */}
+          <Route path="*" element={<Navigate to="/not-found" />} />
         </Routes>
       </Router>
     </div>
