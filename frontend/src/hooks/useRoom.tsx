bacdc69bb7c045e88f5898d6b372f3bf41195075
Manish Sharma <66903126+seeitsmanish@users.noreply.github.com>
2024-07-08 10:09:28 +0530

    Update useRoom.tsx

--- frontend/src/hooks/useRoom.tsx
+++ frontend/src/hooks/useRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useThrottle from "./useThrottle";
 
 export default function useRoom(roomSlug: string) {
