b89d9bea15290b614255f0a18000194532e87b1a
See-Its-Manish <seeitsmanish@gmail.com>
2024-07-27 12:08:00 +0530

    init: aceternity-ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,52 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'dots-pattern': 'radial-gradient(grey 1px, transparent 1px)',
-      },
-      backgroundSize: {
-        'dots-size': '75px 75px',
-      },
-
       keyframes: {
-        slideInFromBottom: {
-          from: {
-            transform: "translateY(25%)",
-            opacity: "0%",
-          },
-          to: {
-            transform: "translateY(0%)",
-            opacity: "100%",
-          }
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        ripple: {
-          to: {
-            transform: "scale(4)",
-            opacity: '0'
-          }
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
-        slideInFromTop: {
-          from: {
-            transform: "translateY(-50%)",
-            opacity: "0%",
-          },
-          to: {
-            transform: "translateY(0%)",
-            opacity: "100%",
-          }
-        }
       },
       animation: {
-        slideInFromBottom: "slideInFromBottom 650ms linear forwards",
-        slideInFromTop: "slideInFromTop 650ms linear forwards",
-        ripple: 'ripple 600ms linear forwards'
-      }
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
