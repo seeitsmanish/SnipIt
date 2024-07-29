452d633b30984db4bfafae9478c1f580192d490e
See-Its-Manish <seeitsmanish@gmail.com>
2024-07-29 23:53:34 +0530

    refactor: Kept all context provider at one place

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ import Room from "./pages/Room";
 import NotFound from "./pages/NotFound";
 import ShootingStars from "./aceternity/components/ui/shooting-stars";
 import { StarsBackground } from "./aceternity/components/ui/stars-background";
-import { LanguageProvider } from "./context/LanguageContext";
+
 import { useRoom } from "./context/RoomContext";
 
 function App() {
@@ -19,23 +19,21 @@ function App() {
   const { isRoom, } = useRoom();
   return (
     <div className={`flex h-[100dvh] w-[100dvw] flex-col justify-between bg-transparent bg-dots-size font-sans relative ${isRoom && "bg-slate-950"}`}>
-      <LanguageProvider>
-        <Nav />
-        <Router>
-          <Routes>
-            {/* Home */}
-            <Route path="/" element={<Home />} />
-            {/* Room */}
-            <Route path="/:roomSlug" element={<Room />} />
-            {/* Not-Found */}
-            <Route path="/not-found" element={<NotFound />} />
-            {/* Redirect unmatched routes to not-found */}
-            <Route path="*" element={
-              <Navigate to="/not-found" />
-            } />
-          </Routes>
-        </Router>
-      </LanguageProvider>
+      <Nav />
+      <Router>
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<Home />} />
+          {/* Room */}
+          <Route path="/:roomSlug" element={<Room />} />
+          {/* Not-Found */}
+          <Route path="/not-found" element={<NotFound />} />
+          {/* Redirect unmatched routes to not-found */}
+          <Route path="*" element={
+            <Navigate to="/not-found" />
+          } />
+        </Routes>
+      </Router>
       {
         !isRoom && (
           <div className="absolute inset-0 z-[-1] bg-slate-950">
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RoomProvider } from "./context/RoomContext.tsx";
+import { UsersProvider } from "./context/UsersContext.tsx";
+import { LanguageProvider } from "./context/LanguageContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <RoomProvider>
-        <App />
+        <UsersProvider>
+            <LanguageProvider>
+                <App />
+            </LanguageProvider>
+        </UsersProvider>
     </RoomProvider>
 );
